2686131cd1bda87629a0770d938cd99ed76ed897
Rishan Menezes <rishanmenezes05@gmail.com>
2024-12-12 01:58:57 +0530

    Updated codes

---
+++ script.js
@@ -0,0 +1,9 @@
+const navLinks = document.querySelectorAll('.navbar a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
